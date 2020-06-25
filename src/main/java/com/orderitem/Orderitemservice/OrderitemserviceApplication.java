02573db8d963c2c2087aa6153400b042c375acca
jyotheswar <jyotheswarmedharamitla@gmail.com>
2020-06-25 17:57:51 +0530

    orderItemService

---
+++ src/main/java/com/orderitem/Orderitemservice/OrderitemserviceApplication.java
@@ -0,0 +1,13 @@
+package com.orderitem.Orderitemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderitemserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderitemserviceApplication.class, args);
+	}
+
+}
